db6463e5f2f847cdf655804c79a83b3983e3fee8
toukali <toukalisamir@outlook.com>
2023-12-09 06:09:12 -0800

    all

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 
+
 def element_at(my_list, idx):
-    if (idx < 0) or (idx > len(my_list) - 1):
-        return None
-    return(my_list[idx])
+    """
+    Gets an element in a list at given index
+    And returns it
+    """
+    list_len = len(my_list)
+    if idx >= list_len or idx < 0:
+        return
+
+    return (my_list[idx])
