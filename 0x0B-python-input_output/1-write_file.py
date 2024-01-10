f24ba298dacfc3fc4a4e323cd7fc05131ddb2a8b
toukali <toukalisamir@outlook.com>
2024-01-10 07:11:02 -0800

    fix task 01

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,7 +2,7 @@
 """This module defines a text file-writting function"""
 
 
-def read_file(filename="",s):
+def read_file(filename="",s=""):
     """write the contents in UTF8 text file"""
-    with open(filename, encoding="utf-8") as f:
+    with open(filename,"w", encoding="utf-8") as f:
         f.write(s)
