aba107b2e4860dacfea28a548885bbb0707214d3
toukali <toukalisamir@outlook.com>
2024-01-19 15:25:29 -0800

    square

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""contains class Square implements class Rectangle"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square implements rectangle"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """initialises Square (overrides Rectangle init)"""
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """returns the size of the square"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """sets the value of size"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+
+        self.width = value
+        self.height = value
