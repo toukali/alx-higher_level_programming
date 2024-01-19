c84d8e94eb0f2c3c265875f23f0b8da526636c55
toukali <toukalisamir@outlook.com>
2024-01-19 13:57:33 -0800

    rectangle validation function

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -56,3 +56,12 @@ class Rectangle(Base):
         """setter function for y"""
         self.validate_int("y", value)
         self.__y = value
+        
+    def validate_int(self, name, value, eq=True):
+        '''Method for validating the value.'''
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if eq and value < 0:
+            raise ValueError("{} must be >= 0".format(name))
+        elif not eq and value <= 0:
+            raise ValueError("{} must be > 0".format(name))
