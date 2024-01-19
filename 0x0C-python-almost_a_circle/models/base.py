4e9555b4a7bb5570abbc67b9e2b6417338da3776
toukali <toukalisamir@outlook.com>
2024-01-19 13:27:43 -0800

    fix error

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 """Defines a base model class."""
+
+
+
 class Base:
     """Represent the base model.
 
@@ -9,11 +12,7 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initialize a new Base.
-
-        Args:
-            id (int): The identity of the new Base.
-        """
+        """Initialize a new Base   """
         if id is not None:
             self.id = id
         else:
