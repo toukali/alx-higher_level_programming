26f1c137709737b0a99824c2461ec1b0da6f6198
toukali <toukalisamir@outlook.com>
2024-01-19 15:04:45 -0800

    rectangle display1

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -75,15 +75,17 @@ class Rectangle(Base):
         rectangle = ""
         print_symbol = "#"
 
-       # for i in range(self.__height-1):
-       #     rectangle += print_symbol * self.__width + "\n"
-       # rectangle += print_symbol * self.__width
+#        for i in range(self.__height - 1):
+#            rectangle += print_symbol * self.__width + "\n"
+#        rectangle += print_symbol * self.__width
 
-       # print("{}".format(rectangle))
-       print("\n" * self.y, end="")
-       for i in range(self.height):
-           rectangle += (" " * self.x) + (print_symbol*self.width) + "\n"
-       print(rectangle, end="")
+#        print("{}".format(rectangle))
+
+        print("\n" * self.y, end="")
+
+        for i in range(self.height):
+            rectangle += (" " * self.x) + (print_symbol*self.width) + "\n"
+        print(rectangle, end="")
         
     def __str__(self):
         """returns a string formart of the rectangle"""
